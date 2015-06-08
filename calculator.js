265fecb4c50658c01aa22d61dd376239398dcb8e
Bryan AA <abrayan87@yahoo.com>
2015-06-08 16:48:27 -0600

    Create calculator.js (This is the calculator)

---
+++ calculator.js
@@ -0,0 +1,7 @@
+//This are the main code of calculator.
+
+function add() { 
+var n1 = parseInt(document.myFormulary.number1.value); 
+var n2 = parseInt(document.myFormulary.number2.value); 
+document.myFormulary.result.value=n1+n2; 
+} 
